12bacdf524fe5866e55359b82fb29d4cea0585b1
Fim1am <drozhanov@mail.ua>
2018-10-11 22:54:48 +0300

    added game grid

---
+++ Assets/Scripts/GameCanvas.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameCanvas : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+    public void GridButton()
+    {
+        FindObjectOfType<GameGrid>().DisplayingGrid();
+    }
+}
---
+++ Assets/Scripts/GameGrid.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class GameGrid : MonoBehaviour
+{
+
+    [SerializeField]
+    private GameObject visualGrid;
+
+    [SerializeField]
+    private CellsLine[] rowsWidth;
+
+    [SerializeField]
+    private CellsLine[] rowsLength;
+
+    [SerializeField]
+    private Cell[] cells;
+
+    private int gridDimension = 18;
+
+    public void DisplayingGrid()
+    {
+        visualGrid.SetActive(!visualGrid.activeSelf);
+    }
+
+}
+
+
+[System.Serializable]
+class CellsLine
+{
+    public Cell[] cells = new Cell[18];
+}
